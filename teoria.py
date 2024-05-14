12b57572fadb12859db36bcb9ef83a11e67fff53
João Guilherme Cavalcanti <joaogui76@gmail.com>
2024-05-14 07:28:45 -0300

    commit inicial

---
+++ teoria.py
@@ -0,0 +1,10 @@
+""" VETORES """
+# Um vetor é uma estrutura de dados linear e ordenada que armazena elementos de um tipo único
+# Cada elemento de um vetor tem índice único que vai de 0 até o tamanho do vetor - 1
+# O tamanho de um vetor é fixo e definido durante sua declaração
+
+vetor = [0, 1, 2, 3, 4, 5] # Um vetor de números inteiros
+
+lista = [0, 1, 'Cavalo', 3.14, ('Sport', 1987)] # Uma lista, pois armazena elementos de diferentes tipos, logo não é vetor
+
+""" MATRIZES """
